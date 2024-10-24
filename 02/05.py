758fcc0e56b9801cc23bf8998bbaaa16525c8854
Kupirden <lihachev.m@mail.ru>
2024-10-24 22:34:22 +0500

    Задачи

--- 02/05.py
+++ 02/05.py
@@ -1,7 +1,7 @@
 word1 = input('Enter a word 1: ')
 word2 = input('Enter a word 2: ')
 word3 = input('Enter a word 3: ')
-if word1 == 'раз' and word2 == 'два' and word3 == 'три':
+if word1 == 'один' or 1 and word2 == 2 and word3 == 3:
     print('гори')
 else:
     print('не гори')
