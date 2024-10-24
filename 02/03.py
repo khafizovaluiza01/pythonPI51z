c097a2cf4f88ba61ac8b68537800d6ff5b17cb75
Kupirden <lihachev.m@mail.ru>
2024-10-24 22:11:43 +0500

    Задачи

---
+++ 02/03.py
@@ -0,0 +1,7 @@
+word1 = input('Enter a word 1: ')
+word2 = input('Enter a word 2: ')
+word3 = input('Enter a word 3: ')
+if word1 == 'раз' and word2 == 'два' and word3 == 'три':
+    print('гори')
+else:
+    print('не гори')
