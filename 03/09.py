9e129830173d7ff898b19cd3f8813f854f924573
Kupirden <lihachev.m@mail.ru>
2024-10-29 22:56:01 +0500

    Задачи

---
+++ 03/09.py
@@ -0,0 +1,10 @@
+boria = int(input())
+vova = int(input())
+dima = int(input())
+
+heights = [boria, vova, dima]
+heights.sort(reverse=True)
+
+print(heights[0])
+print(heights[1])
+print(heights[2])
