24c883f003b3cecb2af6a586bbe5650917d61289
Kupirden <lihachev.m@mail.ru>
2024-10-29 22:15:38 +0500

    Задачи

---
+++ 03/01.py
@@ -0,0 +1,6 @@
+mounth_first = input("Enter a mounth first: ")
+mounth_two = input("Enter a mounth two: ")
+if(mounth_first == "Тула" and mounth_two != "Пенза") or (mounth_first !="Тула" and mounth_two =="Пенза"):
+    print("да ")
+else:
+    print("Нет ")
