0fb9117489f5cf7b9b1a176eb0de856c04b6325d
Kupirden <lihachev.m@mail.ru>
2024-11-03 01:53:15 +0500

    Задачи

--- 05,/01.py
+++ 05,/01.py
@@ -1,10 +1,22 @@
-weather = 0
-num = 0
-while True:
+#weather = 0
+#num = 0
+#while True:
+    #if weather < 22.0:
+        #weather = float(input("Enter a weather: "))
+        #num += 1
+        #print(num)
+    #else:
+        #break
+#print(num//7)
+
+days = int(input('Дни: '))
+count = 0
+for i in range(1, days):
+    weather = float(input("Enter a weather: "))
     if weather < 22.0:
-        weather = float(input("Enter a weather: "))
-        num += 1
-        print(num)
+        print(weather)
+        count +=1
     else:
+        print(count //7)
         break
-print(num//7)
+print('Наблюдения завершены! Количество недель составляет: ', count//7)
