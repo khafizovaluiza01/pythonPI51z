5f408448aad45f94c2159a42d7e3931a6ed18372
Kupirden <lihachev.m@mail.ru>
2024-11-05 21:32:58 +0500

    Задачи

---
+++ 06/02.py
@@ -0,0 +1,11 @@
+n = int(input('Enter a number: '))
+total = False
+for i in range(1, n+1):
+    word = str(input('Enter a word: '))
+    if "Кот" in word or "кот" in word:
+        total = True
+        break
+if total:
+    print('Мяу')
+else:
+    print('Нет')
