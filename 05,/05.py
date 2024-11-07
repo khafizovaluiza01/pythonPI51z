194227c4ec1fd45e1f773f5939d3f6873e12815e
khafizovaluiza01 <khafizovaluiza01@gmail.com>
2024-11-08 01:41:30 +0500

    Delete 05, directory

--- 05,/05.py
+++
@@ -1,6 +0,0 @@
-num_count = 1
-for i in range(6):
-    num = int(input('Enter a number: '))
-    if num !=0:
-        num_count *=abs(num)
-print(num_count)
--- 05,/06.py
+++
@@ -1,3 +0,0 @@
-num = int(input('Enter a number: '))
-for i in range(num+1):
-    print(i, end=' ')
