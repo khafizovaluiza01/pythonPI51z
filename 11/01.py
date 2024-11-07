4f61b51af4b837ea140d168c5fca27d9b3245aa4
Kupirden <lihachev.m@mail.ru>
2024-11-07 23:46:33 +0500

    Задачи

--- 11/01.py
+++ 11/01.py
@@ -1,11 +1,9 @@
 n = int(input('Enter a number: '))
-numbers = []
-
-for i in range(n):
-    numbers.append(int(input('Enter a number list: ')))
+numbers = tuple(int(input('Enter a number list: ')) for _ in range(n)) # Используем генератор списка, чтобы создать кортеж
 
 target_number = int(input())
 found = False
+
 for i in range(n):
     for j in range(i + 1, n):
         if numbers[i] * numbers[j] == target_number:
